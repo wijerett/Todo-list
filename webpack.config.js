89551c8d6b24ab202b048e77fb0ad5751e360f7d
Jeremy Winnett <winnettjeremy@gmail.com>
2026-04-30 20:31:21 -0400

    output to docs

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ export default {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(import.meta.dirname, "dist"),
+    path: path.resolve(import.meta.dirname, "docs"),
     clean: true,
   },
   devtool: "eval-source-map",
